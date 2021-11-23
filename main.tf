1e58c9ea13c77690609fdccab7a54342fa51439e
enriquemanuel <enrique.valenzuela@onepeloton.com>
2021-11-23 16:02:43 -0500

    first pass

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "s3-bucket" {
+	source  = "enriquemanuel.scalr.io/env-tp22m4rk1ci4a0o/s3-bucket/aws"
+	version = "2.11.1"
+
+    bucket = "scalr-demo"
+}
+
+
+module "cloudfront_s3_website_without_domain" {
+   	source  = "enriquemanuel.scalr.io/env-tp22m4rk1ci4a0o/cloudfront-s3-website/aws"
+    version                = "1.2.2"
+    domain_name            = module.s3_bucket.s3_bucket_id // Any random identifier for s3 bucket name
+    use_default_domain     = true
+    upload_sample_file     = true
+}
